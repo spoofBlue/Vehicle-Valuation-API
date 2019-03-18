267629ec959ecff43bfa72dbf2000b467372aeef
Cory Nanni <cory.nanni@gmail.com>
2019-03-18 01:59:31 -0400

    developed for travis CI & heroku deployment, cleaned tester of comments for now.

--- test/valueRouterTester.js
+++ test/valueRouterTester.js
@@ -4,8 +4,6 @@ const chai = require("chai");
 const chaiHttp = require("chai-http");
 const { app, runServer, closeServer } = require("../server");
 
-const { determineValue } = require("../routes/valueRouter");  // !!!!
-
 // Setup Dependencies
 const expect = chai.expect;
 chai.use(chaiHttp);
@@ -22,19 +20,6 @@ const DEFAULTDATA = {
     collisions: 0
 }
 
-// This can be removed.
-function generateQuery(data = DEFAULTDATA) {
-    // Given an object data, returns a string with the key=value data formatted into a url query.  !!!!
-    /*
-    const possibleFactors = [make, model, marketvalue, age, owners, mileage, collisions];
-    return possibleFactors.reduce(function(queryString, factor) {
-        return data.factor ? `${factor}=${data[factor]}&` : ``;
-    }, `?`).slice(-1,1);
-    */
-    return `?make=${data.make}&model=${data.model}&marketvalue=${data.marketvalue}` +
-        `&age=${data.age}&owners=${data.owners}&mileage=${data.mileage}&collisions=${data.collisions}`;
-}
-
 describe("Server Testing", function () {
     before(function () {
         return runServer();
@@ -44,7 +29,7 @@ describe("Server Testing", function () {
     });
 
     it("returns a value, given a valid make, valid model, marketvalue, age, and owners", function () {
-        const query = `?make=honda%20&model=%20civic&marketvalue=1000&age=0&owners=1`
+        const query = `?make=honda&model=civic&marketvalue=1000&age=0&owners=1`
         return chai.request(app)
             .get(`/value${query}`)
             .then(function (res) {
@@ -71,7 +56,6 @@ describe("Server Testing", function () {
             .catch(function (err) {
                 expect(err).to.be.null;
             });
-
     });
 
     it("returns an error due to missing request criteria. 3 missing keys in this case.", function () {
@@ -87,10 +71,5 @@ describe("Server Testing", function () {
             .catch(function (err) {
                 expect(err).to.be.null;
             });
-
     });
-
-
 });
-
-// /value?make=honda&model=civic&marketvalue=1000&age=0&owners=1&mileage=0&collisions=0   !!!
